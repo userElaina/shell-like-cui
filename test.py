3e72ce815a94bc54b54877660451ada8b62ef3ad
Elaina <userelaina@pm.me>
2023-05-03 10:22:39 +0000

    :white_check_mark: update `test.p`

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ from shelllikecui import make_cui1, make_command
 
 a = make_cui1()
 
+
 def qwq(arg: str):
     print('QAQ')
+
 
 a.register(make_command('qwq', qwq, 'qaq', 'QwQ'))
 
@@ -11,4 +13,3 @@ print(a.all_cmd)
 print(a.alias)
 
 a.always_input()
-
