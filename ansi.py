53a6b6322f3e2026eb8916c443616fe0c4b703e1
Elaina <userelaina@pm.me>
2024-01-18 19:31:37 +0000

    :poop:

--- ansi.py
+++ ansi.py
@@ -1,13 +1,15 @@
 s = '123abc'
 
 def p(i: int):
-    print('%03d: \x1b[%dm%s\x1b[0m%s' % (i, i, s, s))
+    print('%03d: \x1b[%dm%s\x1b[0m%s\n' % (i, i, s, s))
 
 
 def t(i: int):
     for i in range(i, i+8):
         p(i)
 
+
+'''
 p(0) # Origin
 p(1) # Bold (or Bright)
 p(3) # Italic
@@ -23,3 +25,10 @@ t(90)
 t(100)
 # *0~*7: black red green yellow blue magenta cyan white
 # 3* 4* 9* 10*: text bg text-bright bg-bright
+
+# 1 (when Bright), 3x = 9x = 1, 9x
+# 1 (when Bold), 3x = 1, 9x; 
+'''
+
+# t(10)
+# t(20)
